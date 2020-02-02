e96ec3cc4c34e4bad428c0352b590055e6617b8c
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-02-02 18:18:09 +0900

    Add url query data

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const app = http.createServer((request, response) => {
-    let { url } = request;
-    if (url == '/') {
-      url = '/index.html';
-    } else if (url == '/favicon.ico') {
+    let requestURL = request.url;
+    const { id = 'index' } = url.parse(requestURL, true).query || {};
+
+    if (requestURL == '/favicon.ico') {
       response.writeHead(404);
       response.end();
       return;
     }
+
+    console.log(`/public/${id}.html`);
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(fs.readFileSync(__dirname + `/public/${id}.html`));
 });
 
 app.listen(3000);
