b15a1ec72be6d20827e7e177fd2de04b78a36954
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-02-13 18:03:52 +0900

    Add error handler

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
+app.get('/test/error', (req, res, next) => {
+  next(new Error("test error!!!"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(3000, () => console.log("Listen port 3000..."));
