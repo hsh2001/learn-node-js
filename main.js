6c309f74f20bb2c706fe8a46c36d807d2936087c
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-02-13 18:53:47 +0900

    Add custom middleware for error handling

--- main.js
+++ main.js
@@ -10,13 +10,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.renderError = (errorCode) => {
+    res.status(errorCode).render('error', { errorCode });
+  };
+  next();
+});
 
 app.use('/', indexRoute);
 app.use('/test', testRoute);
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send('Something broke!');
+  res.renderError(500);
 });
 
 app.listen(3000, () => console.log("Listen port 3000..."));
